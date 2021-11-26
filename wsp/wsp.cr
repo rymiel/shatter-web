b266a00c8c14994a4c0f8f16d2644196119c5e76
Emilia Dreamer <emilia@rymiel.space>
2021-11-26 06:18:26 +0200

    this one shouldn't be here any more

--- wsp/wsp.cr
+++ wsp/wsp.cr
@@ -6,15 +6,7 @@ class RootHandler
 
   def call(context)
     context.request.path = "/index.html" if context.request.path == "/"
-    if context.request.path.starts_with?("/rq") && context.request.method == "POST"
-      r = HTTP::Client.post("https://canary.discord.com/api/webhooks/848887092755038229/_xBrFpw8lcju0gItTZW0bHKIkADb0thN3Nbo1WVaZHe-tVvN5h4fVRdUMA0nXtaYZHrP?wait=true",
-        headers: HTTP::Headers{"Content-Type" => "application/json"},
-        body: {"content" => "access request #{context.request.path}"}.to_json
-      )
-      pp! r unless r.status.ok?
-    else
-      call_next(context)
-    end
+    call_next(context)
   end
 end
 
